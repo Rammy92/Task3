239a14a6978e369f5a5ec28744607babf4e8bbe7
Rammy92 <rammy.mansour@utb.ecutbildning.se>
2020-09-28 22:15:10 +0200

    Iplemented a method that adds number to itself

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,18 @@
 package com.company;
 
 import java.util.Scanner;
+
 import static com.company.GetScanner.getScanner;
 import static com.company.Number.number;
+import static com.company.AddNumbers.addNumbers;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = getScanner();
         number(sc);
-
+        addNumbers();
     }
 
 
