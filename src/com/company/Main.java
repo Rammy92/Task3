6a1b1b08fe84c3dd2107af068571cdc97aafc799
Rammy92 <rammy.mansour@utb.ecutbildning.se>
2020-09-28 18:34:16 +0200

    Add GetScanner method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 
 import java.util.Scanner;
 
+import static com.company.GetScanner.getScanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner sc = getScanner();
 
 
     }
